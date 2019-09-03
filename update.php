8283ead1957dad5cf81cdf2505ef9073cb8036bd
avinash <avinashbendale78@gmail.com>
2019-09-04 00:59:16 +0530

    save

--- update.php
+++ update.php
@@ -1,3 +1,20 @@
+<?php include 'connection.php';
+if(isset($_POST['submit']))
+{
+    $id=GET['id'];
+    $name=$_POST['name'];
+    $address=$_POST['address'];
+    $gender=$_POST['gender'];
+    $mobile=$_POST['mobile'];
+    $email=$_POST['email'];
+    $sql = "update table tblemployee set id=$id,name='$name', address= '$address',gender='$gender',mobile='$mobile',email='email' where id=$id ";
+
+    $query=mysqli_query($conn,$sql);
+    header('location:employees.php');
+}
+
+ ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -21,6 +38,72 @@
     <title>Update</title>
 </head>
 <body>
-    
+<div class="container">
+        <form action="" method="post">
+            <div class="card mt-5">
+                <div class="card-header bg-light">
+                    <h1 class="text-primary text-center">EMPLOYEE</h1>
+                </div>
+                <div class="card-body" style="place-content:center;">
+                    <div class="form-group row">
+                        <div class="col-md-2">
+                            <label for="validationCustom01" class="col-form-label">Name</label>
+                        </div>
+                        <div class="col-md-4">
+                            <input type="text" class="form-control" name="name" placeholder="Name" required>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-md-2">
+                            <label for="validationCustom01" class="col-form-label">Address</label>
+                        </div>
+                        <div class="col-md-4">
+                        <textarea class="form-control" name="address" rows="3" placeholder="Address"></textarea>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-md-2">
+                            <label for="validationCustom01" class="col-form-label">Gender</label>
+                        </div>
+                        <div class="col-md-4"><br />
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" name="gender" value="male">
+                                <label class="form-check-label" for="inlineRadio1">Male</label>
+                            </div>
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" name="gender" value="female">
+                                <label class="form-check-label" for="inlineRadio2">Female</label>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-md-2">
+                            <label for="validationCustom01" class="col-form-label">Mobile</label>
+                        </div>
+                        <div class="col-md-4">
+                            <input type="text" class="form-control" name="mobile" placeholder="Mobile" required>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-md-2">
+                            <label for="validationCustom01" class="col-form-label">Email</label>
+                        </div>
+                        <div class="col-md-4">
+                            <input type="email" class="form-control" name="email" placeholder="Email" required>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-md-2">
+
+                        </div>
+                        <div class="col-md-4">
+                            <button type="submit" class="btn btn-success" value="submit" name="submit">Submit</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
